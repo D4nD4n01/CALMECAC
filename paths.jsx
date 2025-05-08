8f21784aded1183c4200592867ee346ea2675bda
D4nD4n01 <danytazos@gmail.com>
2025-05-08 13:35:58 -0600

    20250508 1335

--- paths.jsx
+++ paths.jsx
@@ -1,7 +1,8 @@
 // archivo de rutas.js
 const Rutas = {
     URL: "https://calmecac-backend-production.up.railway.app/",
-    LOGIN: "login"
+    LOGIN: "login",
+    COURSE: "course"
   };
   
   export default Rutas;
